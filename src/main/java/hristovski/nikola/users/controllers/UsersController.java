fa3d91957b9f5c2c679d395720139143a3cb07db
Nikola Hristovski <nikolahristovski1122@gmail.com>
2020-04-03 22:37:10 +0200

    Heroku

--- src/main/java/hristovski/nikola/users/controllers/UsersController.java
+++ src/main/java/hristovski/nikola/users/controllers/UsersController.java
@@ -4,7 +4,10 @@ import hristovski.nikola.users.exceptions.FailedToCreateUserException;
 import hristovski.nikola.users.exceptions.NoUserFoundException;
 import hristovski.nikola.users.models.RegisterRequest;
 import hristovski.nikola.users.security.models.ApplicationUser;
+import hristovski.nikola.users.security.models.Role;
+import hristovski.nikola.users.security.models.RoleName;
 import hristovski.nikola.users.security.repositories.ApplicationUserRepository;
+import hristovski.nikola.users.security.repositories.RoleRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.ConversionService;
@@ -12,7 +15,10 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import javax.validation.Valid;
+import java.util.Optional;
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -20,7 +26,37 @@ import javax.validation.Valid;
 public class UsersController {
 
     private final ApplicationUserRepository applicationUserRepository;
+    private final RoleRepository roleRepository;
     private final ConversionService conversionService;
+
+
+    @PostConstruct
+    public void init(){
+        Optional<Role> roleUser = roleRepository.findByName(RoleName.ROLE_USER);
+        if (roleUser.isEmpty()){
+            roleRepository.save(new Role(1L,RoleName.ROLE_USER));
+        }
+
+        Optional<Role> roleAdmin = roleRepository.findByName(RoleName.ROLE_ADMIN);
+        if (roleAdmin.isEmpty()){
+            roleRepository.save(new Role(2L,RoleName.ROLE_ADMIN));
+        }
+
+        Optional<ApplicationUser> admin = applicationUserRepository.findByUsername("admin");
+
+        if (admin.isEmpty()) {
+            RegisterRequest rr = new RegisterRequest("admin", "admin", "admin@gmail.com", "admin", "admin");
+
+            Set<Role> roles = Set.of(roleRepository.findByName(RoleName.ROLE_USER).get(),
+                    roleRepository.findByName(RoleName.ROLE_ADMIN).get());
+            ApplicationUser user = conversionService.convert(rr, ApplicationUser.class);
+
+            user.setRoles(roles);
+            applicationUserRepository.save(user);
+            log.info("Added admin user");
+        }
+    }
+
 
     @PostMapping("/register")
     public ResponseEntity register(@Valid @RequestBody RegisterRequest registerRequest) {
